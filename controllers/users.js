c99ad6dd704ce7065f821f14f9445815a3a0d38d
Tyson Truedell <truedet@yahoo.com>
2024-10-17 22:26:42 -0400

    feat: creating routes and controllers for users

---
+++ controllers/users.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const router = express.Router();
+
+const User = require("../models/user");
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find(); // Fetch all users
+    res.status(200).json(users); // Return users as JSON
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching users" });
+  }
+};
+
+const getUser = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user" });
+  }
+};
+
+const createUser = async (req, res) => {
+  const { name, avatar } = req.body;
+  try {
+    const newUser = new User({ name, avatar });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error creating user", error: error.message });
+  }
+};
+
+router.get("/users", getUsers);
+router.get("/users/:userId", getUser);
+router.post("/users", createUser);
+
+module.exports = router;
