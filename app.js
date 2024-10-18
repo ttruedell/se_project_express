17abb2e2db7188c7938265cc05d316af61db41d1
Tyson Truedell <truedet@yahoo.com>
2024-10-17 21:57:02 -0400

    creating User and and Clothing Item schemas and models

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const { PORT = 3001 } = process.env;
+
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 const app = express();
 
