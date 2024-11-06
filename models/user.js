077af6109cebb7a96d6643f650d7993aa4d8d939
Tyson Truedell <truedet@yahoo.com>
2024-11-06 14:22:46 -0500

    Adding email and password fields to userSchema

--- models/user.js
+++ models/user.js
@@ -19,6 +19,21 @@ const userSchema = new mongoose.Schema({
       message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 10,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
