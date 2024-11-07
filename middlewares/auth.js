064da0c49b9518bc6fe83dd54c7babbc27497a60
Tyson Truedell <truedet@yahoo.com>
2024-11-07 14:18:54 -0500

    creating middleware for authorization

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+
+const { AUTHENTICATION_ERROR } = require("../utils/errors");
+
+const JWT_SECRET = require("../utils/config");
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res
+      .status(AUTHENTICATION_ERROR)
+      .send({ message: "Authorization required" });
+  }
+
+  const token = authorization.replace("Bearer ", "");
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res
+      .status(AUTHENTICATION_ERROR)
+      .send({ message: "Authorization required" });
+  }
+
+  req.user = payload;
+
+  next();
+};
