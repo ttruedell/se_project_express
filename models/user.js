ad0e0343b87dbe899ef653b3cc407537244de3d9
Tyson Truedell <truedet@yahoo.com>
2024-11-07 20:43:53 -0500

    setting select property for password to false; returning password hash after calling model method

--- models/user.js
+++ models/user.js
@@ -35,6 +35,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 10,
+    select: false,
   },
 });
 
@@ -42,18 +43,20 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password
 ) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error("Incorrect email or password."));
-    }
-
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error("Incorrect email or password."));
       }
-      return user;
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password."));
+        }
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model("User", userSchema);
