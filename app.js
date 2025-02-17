00db1d6cd8ad0b13a334dc66092ddd526ab2e6e5
Tyson Truedell <truedet@yahoo.com>
2025-02-17 08:21:32 -0500

    adding handler for unhandled error, allowing request request without restarting application manually

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use(requestLogger);
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 
 app.use("/", routes);
 
