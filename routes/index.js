9137ffd6a301a9b3006df294c437e29201c2b58a
Tyson Truedell <truedet@yahoo.com>
2025-02-17 20:16:51 -0500

    applying throw for router.use() to centralized error handler

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ const {
 
 const { login, createUser } = require("../controllers/users");
 
-const ERROR_CODES = require("../utils/errors");
+// const ERROR_CODES = require("../utils/errors");
+const NotFoundError = require("../utils/errors/NotFoundError");
 
 router.use("/", userRoutes);
 router.use("/items", clothingItemRoutes);
@@ -23,9 +24,10 @@ router.post("/signup", validateUserSignup, createUser);
 
 router.use((req, res) => {
   console.error();
-  res
-    .status(ERROR_CODES.NOT_FOUND)
-    .json({ message: "Requested resource not found" });
+  // res
+  //   .status(ERROR_CODES.NOT_FOUND)
+  //   .json({ message: "Requested resource not found" });
+  return next(new NotFoundError("Requested resource not found."));
 });
 
 module.exports = router;
