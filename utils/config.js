ec6a5db042773fc61ec19630976a0636bf3aa74c
Tyson Truedell <truedet@yahoo.com>
2025-02-17 20:33:07 -0500

    adjusting secret key to match server

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,5 @@
-const { JWT_SECRET = "your-secret-key-here" } = process.env;
+const {
+  JWT_SECRET = "f8a1c3d27e4b91ab89fd86c7b14f8a02d3b5ef891c463b8fbe1c7a4899f3a6f1",
+} = process.env;
 
 module.exports = { JWT_SECRET };
