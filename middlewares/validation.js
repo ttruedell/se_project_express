31896b3ba919e82f5a9dcb457446b242e0b91784
Tyson Truedell <truedet@yahoo.com>
2025-02-18 13:25:59 -0500

    adding body object to wrap the fields

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -61,14 +61,16 @@ const validateUserLogin = celebrate({
 });
 
 const validateProfileUpdate = celebrate({
-  name: Joi.string().min(2).max(30).messages({
-    "string.min": 'The minimum length of the "name" field is 2',
-    "string.max": 'The maximum length of the "name" field is 30',
-    "string.empty": 'The "name" field must be filled in',
-  }),
-  avatar: Joi.string().required().custom(validateURL).messages({
-    "string.empty": 'The "avatar" field must be filled in',
-    "string.uri": 'The "avatar" field must be a valid URL',
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).messages({
+      "string.min": 'The minimum length of the "name" field is 2',
+      "string.max": 'The maximum length of the "name" field is 30',
+      "string.empty": 'The "name" field must be filled in',
+    }),
+    avatar: Joi.string().required().custom(validateURL).messages({
+      "string.empty": 'The "avatar" field must be filled in',
+      "string.uri": 'The "avatar" field must be a valid URL',
+    }),
   }),
 });
 
